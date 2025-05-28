fd8b5d576e756a009fefa748c23386a6b0e1041d
Trung <baotrung.nguyen.10f@gmail.com>
2025-05-28 15:13:12 +0200

    feat(webapp): implement ingredients connectors

--- apps/cooquoi-webapp/app/page.tsx
+++ apps/cooquoi-webapp/app/page.tsx
@@ -1,3 +1,15 @@
-export default function Page() {
-  return <h1>Hello, Next.js!</h1>
+import { IngredientsConnector } from "../api-connectors/ingredients-connector";
+
+const ingredientsConnector = new IngredientsConnector();
+
+export default async function Page() {
+	const ingredients = await ingredientsConnector.getIngredients();
+	return (
+		<>
+			<h1>Hello, Next.js!</h1>
+			{ingredients.map(({ id, name }) => (
+				<div key={id}>{name}</div>
+			))}
+		</>
+	);
 }
